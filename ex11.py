69f0f61a709eb4c70ef83fa18eb97c901bed66bc
heathershockney <shockney@atlanticbb.net>
2017-06-28 17:01:03 -0400

    questions

---
+++ ex11.py
@@ -0,0 +1,9 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "What is favorite color?",
+color = raw_input()
+
+print "So, you're %r old, %r tall and %r is a great color." % (
+    age, height, color)
